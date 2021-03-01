47a8dc613a05e8d5890ddb04b52d3c92ce5fd23c
Quentin Bigouraux <quentin.bigouraux@cpe.fr>
2021-03-01 12:04:37 +0100

    dernière modif

--- Analyse_de_donnees.py
+++ Analyse_de_donnees.py
@@ -9,3 +9,60 @@
 #    Balises favorites
 #    ...
 
+#support
+
+from sklearn import tree
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+import graphviz
+import pydotplus
+import numpy as np 
+from IPython.display import Image, display
+import pandas as pd
+
+Image_de_donnee = pd.read_csv("./pokemon.csv")
+
+print(Image_de_donnee)
+
+choix_de_utilisateur = [ #devrat être remplis par un utilisateur
+              'Favorite',
+              'NotFavorite',
+              'Favorite',
+              'Favorite',
+              'Favorite',
+              ]
+"""
+#creating dataframes
+dataframe = pd.DataFrame(data, columns=['color', 'tag', 'size', 'mode'])
+resultframe = pd.DataFrame(result, columns=['favorite'])
+
+#generating numerical labels
+le1 = LabelEncoder()
+dataframe['color'] = le1.fit_transform(dataframe['color'])
+
+le2 = LabelEncoder()
+dataframe['tag'] = le2.fit_transform(dataframe['tag'])
+
+le3 = LabelEncoder()
+dataframe['size'] = le3.fit_transform(dataframe['size'])
+
+le4 = LabelEncoder()
+dataframe['mode'] = le4.fit_transform(dataframe['mode'])
+
+le5 = LabelEncoder()
+resultframe['favorite'] = le5.fit_transform(resultframe['favorite'])
+
+#Use of random forest classifier
+rfc = RandomForestClassifier(n_estimators=10, max_depth=2,
+                  random_state=0)
+rfc = rfc.fit(dataframe, resultframe.values.ravel())
+
+#prediction
+prediction = rfc.predict([
+        [le1.transform(['red'])[0], le2.transform(['nature'])[0],
+         le3.transform(['thumbnail'])[0], le4.transform(['portrait'])[0]]])
+print(le5.inverse_transform(prediction))
+print(rfc.feature_importances_)
+"""
+
