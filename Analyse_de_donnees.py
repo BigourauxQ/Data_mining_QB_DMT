138a5f2a114720bcec267a59358bdf2ce28df716
Quentin Bigouraux <quentin.bigouraux@cpe.fr>
2021-03-01 11:18:53 +0100

    new

---
+++ Analyse_de_donnees.py
@@ -0,0 +1,11 @@
+#Demandez à l'utilisateur de sélectionner quelques images et d'ajouter des balises. Pour chaque utilisateur, 
+#vous êtes maintenant prêt à construire un profil de préférences d'utilisateur,
+#basé sur cette sélection. Vous pouvez recueillir les informations suivantes manuellement,
+#mais l'objectif de cette tâche consiste à les obtenir en utilisant les images sélectionnées de manière automatisée :
+
+#    Couleurs préférées
+#    Orientation de l'image préférée
+#    Tailles d'images préférées (vignettes, grandes images, images de taille moyenne images, etc.)
+#    Balises favorites
+#    ...
+
--- ReadMe.md
+++ ReadMe.md
@@ -1,10 +1,13 @@
 
 gauthier:
+
 automatisation téléchargement images (ft planche)
 Étiquetage et annotation
 Visualisation des données
+Tkinter (a la fin UIT)
 
 Quentin:
+
 Analyses de données
 Système de recommandation
 
