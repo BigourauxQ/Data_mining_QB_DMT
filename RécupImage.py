9d0487e2b7a91653d8fa5178960bbf30df3bdba9
Vango199 <62698943+Vango199@users.noreply.github.com>
2021-03-17 12:20:31 +0100

    Enfin j arrive a dl les donnees et les images

--- RécupImage.py
+++ RécupImage.py
@@ -5,13 +5,14 @@ import shutil
 from os.path  import basename
 import re
 from urllib.request import Request, urlopen
+from PIL import Image
 
 import requests;
-
+#https://img.pokemondb.net/artwork/large/ninjask.jpg
 
 def getURLImg():
 
-    url='https://pokemondb.net/sprites'
+    url='https://pokemondb.net/pokedex/stats/height-weight'
     req = Request(url, headers={'User-Agent': 'XYZ/3.0'})
     codeHTML = urlopen(req, timeout=20).read()
     
@@ -21,26 +22,41 @@ def getURLImg():
     soup = BeautifulSoup(codeHTML)
     
     
-    images = soup.findAll("div", {"class":"infocard-list infocard-list-pkmn-sm"})
-    print(images)
+    images = soup.findAll('tr')
+    #print(images)
     listeimg=[]
     for img in images:
-        image=img.find("span").get('data-src')
-        listeimg.append(image)
-    print(listeimg[0])       
+        
+        image = img.find_all('td')
+        if len(image) == 0:
+            continue
+        image=image[1].getText().lower()
+        if ' ' not in image and '♀' not in image and '♂' not in image and "'" not in image:
+            #print(image)
+            
+            listeimg.append(image)
+    #print(listeimg[0])       
    
 
     return listeimg
 
-def saveImg(url_Img): 
+def saveImg(url_Img):
+
+    #shutil.rmtree('Images2')
+
+
     try:
-        os.mkdir('Images2')
+            os.mkdir('Images2')
     except FileExistsError:
-        print("fichier déjà crée")
-    
-    f = open('Images2/2.jpg','wb')
-    f.write(requests.get(url_Img[0]).content)
-    f.close()
+            print("fichier déjà crée")
+    for i in range(0,898): 
+        f = open('Images2/'+url_Img[i]+'.jpg','wb')
+        #print('https://img.pokemondb.net/artwork/large/'+url_Img[i]+'.jpg')
+        f.write(requests.get('https://img.pokemondb.net/artwork/large/'+url_Img[i]+'.jpg').content)
+        im=Image.open('Images2/'+url_Img[i]+'.jpg')
+        _, ext = os.path.splitext('Images2/'+url_Img[i]+'.jpg')
+        #print(ext)
+        f.close()
 
    
     #shutil.move("2.jpg", "Images2" )
@@ -52,7 +68,7 @@ def saveImg(url_Img):
 
 #urlimg = getURLImg('https://commons.wikimedia.org/wiki/Main_Page')
 
-getURLImg()
+saveImg(getURLImg())
 
 print("end")
 
--- test.py
+++ test.py
@@ -1,32 +1,54 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import os
+import sys
+import shutil
+from os.path  import basename
+import re
+from urllib.request import Request, urlopen
+from PIL import Image
+import csv
+import requests;
+#https://img.pokemondb.net/artwork/large/ninjask.jpg
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC
+def getURLImg():
 
+    url='https://pokemondb.net/pokedex/stats/height-weight'
+    req = Request(url, headers={'User-Agent': 'XYZ/3.0'})
+    codeHTML = urlopen(req, timeout=50).read()
+    
+    codeHTML=codeHTML.decode("utf-8")
+    
+    
+    soup = BeautifulSoup(codeHTML)
+    
+    
+    results = soup.findAll('tr')
+    rows = []
+    rows.append(['id', 'Name', 'Type', 'height', 'poids'])
+    # loop over results
+    for result in results:
+        # find all columns per result
+        data = result.find_all('td')
+        # check that columns have data
+        if len(data) == 0:
+            continue
+    
+        rank = data[0].getText()
+        name = data[1].getText().lower()
+        type =data[2].getText()
+        height=data[4].getText()
+        poids=data[5].getText()
+        #print(height)
+        if ' ' not in name and '♀' not in name and '♂' not in name and "'" not in name:
+            rows.append([rank,name, type, height,poids])
+        with open('DataPokemon.csv','w', newline='') as f_output:
+            csv_output = csv.writer(f_output)
+            csv_output.writerows(rows)
 
-import urllib.request
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import time
-import pandas as pd# specify the url
+###########Main##############
 
-options = Options()
-options.add_argument('--headless')
-#options.add_argument('--disable-gpu')  # maybe needed if running on Windows.
+#urlimg = getURLImg('https://commons.wikimedia.org/wiki/Main_Page')
 
-urlpage = 'https://www.kaggle.com/vishalsubbiah/pokemon-images-and-types' 
-print(urlpage)
-# run firefox webdriver from executable path of your choice
-driver = webdriver.Chrome(chrome_options=options)
-# get web page
-driver.get(urlpage)
-# execute script to scroll down the page
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-# sleep for 30s
-time.sleep(10)
-# driver.quit()
-results = driver.find_elements_by_class_name("mdc-menu-surface--anchor")
-print('Number of results', len(results), results[0].find_element_by_tag_name('div'))
+table = getURLImg()
+
+print("end")
